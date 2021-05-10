6dc9b1338d7d116ff575f33e67735d32b153de07
Max Carlsson <max.h.s.carlsson@gmail.com>
2021-05-10 17:53:08 +0200

    Environment variable fixed

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print(os.environ['SECRET_KEY'])
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
